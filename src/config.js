af340cbd5648286625987cfcaaffc63c85a3af4b
aamirnazeer <aamirnazeerbhat@gmail.com>
2022-05-23 21:22:41 +0530

    fixed config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,13 @@
 const hostname = window && window.location && window.location.hostname;
 
 const projectAPI = () => {
-  const apiURL = "http://localhost:3001";
+
+  if (hostname ===  'localhost:3000') {
+    apiURL = "http://localhost:3001";
+  }
   // const apiURL = 'https://fullstackprojectapi.herokuapp.com'
 
-  if (hostname === "https://fullstackproject-peach.vercel.app") {
+  if (hostname === "fullstackproject-peach.vercel.app") {
     apiURL = "https://fullstackprojectapi.herokuapp.com";
   }
   return apiURL;
